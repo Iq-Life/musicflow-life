9de28212abbd119f81ad00c3f2a6285d86ed5874
Pavel Vavilin <iq23life@gmail.com>
2023-10-24 21:04:20 +0300

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import style from './page.module.scss'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={style.contact}>
+      <h1>I`m interested in your thoughts</h1>
+      <div className={style.content}>
+        <div className={style.containerIcon}>photo</div>
+        <form name='form'>
+          <input name='name' type='text' placeholder='name'></input>
+          <input name='email' type='text' placeholder='email'></input>
+          <textarea
+            name='message'
+            placeholder='message'
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button type='submit'>send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
